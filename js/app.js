fe6a39f04b5e923f7cfe7cdad063b2392f10cd82
Andrea Leoncini <andrea.leoncini@gmail.com>
2019-05-17 19:17:44 +0200

    work on trips page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,56 @@
-var RS_MRTDB = 'http://mrtdb-mrtool.127.0.0.1.nip.io/';
+var locationName = '';
+var distance = 0;
+var numberOfTrips = 0;
+var totalMileage = 0;
 
 function checkUser() {
-  if (! localStorage.rhid) {
+  if (localStorage.rhid.length > 3) {
+    $('#userName').text(localStorage.name);
+  } else {
     $('#enterAssociateInfoModal').modal('show');
   }
+};
+
+function initLocations() {
+  $.ajax({
+      url: '/rs/locations',
+      type: 'GET',
+      data: {},
+      dataType: 'json',
+      complete: function(response, status, xhr){
+          var data = jQuery.parseJSON(response.responseText);
+          formatLocationList(data.locations);
+          //$("#the_response").text(data.result);
+      }
+  });
+};
+
+function formatLocationList(locations) {
+  $('#tList').empty();
+  var options = "";
+  $.each(locations, function (index, location) {
+      options += "<option value='" + location.destination + "'>" + location.distance + "</option>";
+  });
+  $('#tList').append(options);
+};
+
+function saveTrip() {
+  var data = '{ "associateId": "' + localStorage.rhid + '", ';
+  data += '"purpose": "' + $('#inputPurpose').val() + '", ';
+  data += '"location": { "destination": "' + locationName + '", "distance": ' + distance + '}, ';
+  data += '"date": { "day": ' + $('#inputDay').val() + ', "month": ' + $('#inputMonth :selected').val() + ', "year": ' + $('#inputYear :selected').val() + ' } ';
+  data += '}';
+  console.log(data);
+  $.ajax({
+        type: "POST",
+        url: "/rs/trips/add",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){
+          addTripToTable();
+        }
+  });
 };
 
 function saveUser() {
@@ -22,21 +69,23 @@ function saveUser() {
   data += '}';
   $.ajax({
         type: "POST",
-        url: RS_MRTDB + "rs/associates/add",
+        url: "/rs/associates/add",
         data: data,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(data){
-          alert(data);
-        },
+          localStorage.rhid = $('#inputAssociateId').val();
+          localStorage.name = $('#inputAssociateName').val();
+          alert("The tool is now setup to work with RHID: " + localStorage.rhid + ". If you want to act as different user go to setup page and save new info.");
+          window.location="index.html";
+        }
   });
 };
 
 function initTripsTable() {
-    var rhid = "9053";
     var month = $('#inputMonth').val();
     var year = $('#inputYear').val();
-    var theUrl = '/rs/trips/' + rhid + "/" + year + "/" + month;
+    var theUrl = '/rs/trips/' + localStorage.rhid + "/" + year + "/" + month;
     $.ajax({
         url: theUrl,
         type: 'GET',
@@ -51,8 +100,6 @@ function initTripsTable() {
 };
 
 function formatTripsTable(trips) {
-    var numberOfTrips = 0;
-    var totalMileage = 0;
     var tableContent = '<thead>';
     tableContent += '<tr><th>Day</th><th>From office to</th><th>purpose</th><th>mileage (Km)</th></tr>';
     tableContent += '</thead>';
@@ -69,9 +116,23 @@ function formatTripsTable(trips) {
     });
     tableContent += '</tbody>';
 
-    var tableInfo = '<p><img src="images/transparent_spacer.png"><strong>' + $("#inputMonth option:selected").text() + ' ' + $("#inputYear option:selected").text() + '</strong>';
-    tableInfo += '- Number of Trips: <strong>' + numberOfTrips + '</strong>';
-    tableInfo += ' - Total mileage (Km): <strong>' + totalMileage + '</strong>';
-    document.getElementById("tableInfo").innerHTML = tableInfo;
+    document.getElementById("bannerMonth").innerHTML = $("#inputMonth option:selected").text();
+    document.getElementById("bannerYear").innerHTML = $("#inputYear option:selected").text();
+    document.getElementById("bannerNumberOfTrips").innerHTML = numberOfTrips;
+    document.getElementById("bannerTotalMIleage").innerHTML = totalMileage;
     document.getElementById("tbl_round_list").innerHTML = tableContent;
+};
+
+function addTripToTable() {
+  var rowContent = '<tr>';
+  rowContent += '<td>' + $('#inputDay :selected').val() + '</td>';
+  rowContent += '<td>' + locationName + '</td>';
+  rowContent += '<td>' + $('#inputPurpose').val() + '</td>';
+  rowContent += '<td>' + distance + '</td>';
+  rowContent += '</tr>';
+  numberOfTrips += 1;
+  totalMileage += parseInt(distance, 10);
+  document.getElementById("bannerNumberOfTrips").innerHTML = numberOfTrips;
+  document.getElementById("bannerTotalMIleage").innerHTML = totalMileage;
+  $('#tbl_round_list > tbody:last-child').append(rowContent);
 };
